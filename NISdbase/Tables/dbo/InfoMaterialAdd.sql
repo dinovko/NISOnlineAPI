9659a8c7152301dbba21aafc1c00452bf1426a0c
Ravil <garaev.rav@gmail.com>
2021-03-25 12:56:14 +0600

    db

--- NISdbase/Tables/dbo/InfoMaterialAdd.sql
+++ NISdbase/Tables/dbo/InfoMaterialAdd.sql
@@ -4,9 +4,13 @@
     [ClassID] INT NULL, 
     [SubjectNameID] INT NULL, 
     [UserID] INT NULL, 
+    [SectionNameID] INT NULL, 
+    [ThemeNameID] INT NULL, 
     CONSTRAINT [PK_InfoMaterialAdd] PRIMARY KEY CLUSTERED ([ID] ASC) WITH (FILLFACTOR = 90),
     CONSTRAINT [FK_InfoMaterialAdd_NSIClassName] FOREIGN KEY ([ClassID]) REFERENCES [dbo].[NSIClassName] ([ID]),
-    CONSTRAINT [FK_InfoMaterialAdd_NSISubjectName] FOREIGN KEY ([SubjectNameID]) REFERENCES [dbo].[NSISubjectName] ([ID])
+    CONSTRAINT [FK_InfoMaterialAdd_NSISubjectName] FOREIGN KEY ([SubjectNameID]) REFERENCES [dbo].[NSISubjectName] ([ID]),
+    CONSTRAINT [FK_InfoMaterialAdd_NSISectionName] FOREIGN KEY ([SectionNameID]) REFERENCES [dbo].[NSISectionName] ([ID]),
+    CONSTRAINT [FK_InfoMaterialAdd_NSIThemeName] FOREIGN KEY ([ThemeNameID]) REFERENCES [dbo].[NSIThemeName] ([ID])
 )
 GO
 EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Классификация материала', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'InfoMaterialAdd';
--- NISdbase/Tables/dbo/TestData.sql
+++ NISdbase/Tables/dbo/TestData.sql
@@ -4,9 +4,13 @@
     [ClassID] INT NULL, 
     [SubjectNameID] INT NULL, 
     [UserID] INT NULL, 
+    [SectionNameID] INT NULL, 
+    [ThemeNameID] INT NULL, 
     CONSTRAINT [PK_TestData] PRIMARY KEY CLUSTERED ([ID] ASC) WITH (FILLFACTOR = 90),
     CONSTRAINT [FK_TestData_NSIClassName] FOREIGN KEY ([ClassID]) REFERENCES [dbo].[NSIClassName] ([ID]),
-    CONSTRAINT [FK_TestData_NSISubjectName] FOREIGN KEY ([SubjectNameID]) REFERENCES [dbo].[NSISubjectName] ([ID])
+    CONSTRAINT [FK_TestData_NSISubjectName] FOREIGN KEY ([SubjectNameID]) REFERENCES [dbo].[NSISubjectName] ([ID]),
+    CONSTRAINT [FK_TestData_NSISectionName] FOREIGN KEY ([SectionNameID]) REFERENCES [dbo].[NSISectionName] ([ID]),
+    CONSTRAINT [FK_TestData_NSIThemeName] FOREIGN KEY ([ThemeNameID]) REFERENCES [dbo].[NSIThemeName] ([ID])
 )
 GO
 EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Классификация теста', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'TestData';
