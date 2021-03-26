6739ebf4c5eeb15a4119cdf1d11f59e98aee3e59
Ravil <garaev.rav@gmail.com>
2021-03-26 17:49:33 +0600

    ---

---
+++ WebAPI/Controllers/MaterialDataController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Models;
+using WebAPI.Models.TableModel;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MaterialDataController : ControllerBase
+    {
+        ContextDB db;
+
+        public MaterialDataController(ContextDB context)
+        {
+            db = context;
+        }
+
+        [HttpPost("saveMaterialData")]
+        public async Task<ActionResult<InfoMaterialAdd>> Post(InfoMaterialAdd info)
+        {
+            try
+            {
+                if (info == null)
+                {
+                    return BadRequest();
+                }
+                //db.InfoMaterialAdd.Add(info);
+                await db.SaveChangesAsync();
+                return Ok(info);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
